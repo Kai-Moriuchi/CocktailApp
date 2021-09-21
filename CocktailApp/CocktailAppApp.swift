64e1b40bc0d960345117a9303dc04c2d593408d3
Kai-Moriuchi <g2121053@fun.ac.jp>
2021-09-21 16:48:10 +0900

    Initial Commit

---
+++ CocktailApp/CocktailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailAppApp.swift
+//  CocktailApp
+//
+//  Created by Kai on 2021/09/21.
+//
+
+import SwiftUI
+
+@main
+struct CocktailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
